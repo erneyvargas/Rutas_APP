74766023d918bd2fef52a86f5687aaed3bc1bfbc
Erney Vargas <erney.vargas@mct.com.co>
2021-10-10 00:17:43 -0500

    Seguir la ubicacion del usuario

--- lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
+++ lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
 
@@ -6,9 +9,27 @@ part 'mi_ubicacion_event.dart';
 part 'mi_ubicacion_state.dart';
 
 class MiUbicacionBloc extends Bloc<MiUbicacionEvent, MiUbicacionState> {
-  MiUbicacionBloc() : super(MiUbicacionState()) {
-    on<MiUbicacionEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  MiUbicacionBloc() : super(MiUbicacionState());
+
+  final _geloacator = new Geolocator();
+  StreamSubscription<Position> _positionSubscription;
+
+  void iniciarSeguiminto() {
+    // validar la precicion de la ubicacion,
+    final geoLocatorOptions = LocationOptions(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 10,
+    );
+
+    this._positionSubscription =
+        this._geloacator.getPositionStream(geoLocatorOptions).listen(
+      (Position position) {
+        print(position);
+      },
+    );
+  }
+
+  void cancelarSeguimiento() {
+    this._positionSubscription?.cancel();
   }
 }
