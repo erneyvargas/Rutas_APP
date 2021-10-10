c880f1b79ceb1c713d939d2838f6632d41cb510c
Erney Vargas <erney.vargas@mct.com.co>
2021-10-09 23:32:45 -0500

    Configuracion de bloc como amnejador de estado, creacion del bloc ubicacion

---
+++ lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:meta/meta.dart';
+
+part 'mi_ubicacion_event.dart';
+part 'mi_ubicacion_state.dart';
+
+class MiUbicacionBloc extends Bloc<MiUbicacionEvent, MiUbicacionState> {
+  MiUbicacionBloc() : super(MiUbicacionState()) {
+    on<MiUbicacionEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/bloc/mi_ubicacion/mi_ubicacion_state.dart
@@ -0,0 +1,26 @@
+part of 'mi_ubicacion_bloc.dart';
+
+@immutable
+class MiUbicacionState {
+  final bool siguiendo;
+  final bool existeUbicacion;
+  final LatLng ubicacion;
+
+  MiUbicacionState({
+    this.siguiendo = true,
+    this.existeUbicacion = false,
+    this.ubicacion,
+  });
+
+  // Hacer copia del estado con cada uno de los valores del state para enviar
+  MiUbicacionState copyWith({
+    final bool siguiendo,
+    final bool existeUbicacion,
+    final LatLng ubicacion,
+  }) =>
+      new MiUbicacionState(
+        siguiendo: siguiendo ?? this.siguiendo,
+        existeUbicacion: existeUbicacion ?? this.existeUbicacion,
+        ubicacion: ubicacion ?? this.ubicacion,
+      );
+}
