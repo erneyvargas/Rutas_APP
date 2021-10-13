eeb5d85d623b47e79c1f62de9dd67fbeb99f3c1a
Erney Vargas <erney.vargas@mct.com.co>
2021-10-13 16:49:52 -0500

    Sombra y Rectangulo de custom Marker

--- lib/custom_markers/marker_inicio.dart
+++ lib/custom_markers/marker_inicio.dart
@@ -23,6 +23,23 @@ class MarkerInicioPainter extends CustomPainter {
       circuloBalncoR,
       paint,
     );
+
+    // Sombra
+    final Path path = new Path();
+    path.moveTo(40, 20);
+    path.lineTo(size.width - 10, 20);
+    path.lineTo(size.width - 10, 100);
+    path.lineTo(40, 100);
+    canvas.drawShadow(path, Colors.black87, 10, false);
+
+    // Caja Blanca
+    final cajaBlanca = Rect.fromLTWH(40, 20, size.width - 55, 80);
+    canvas.drawRect(cajaBlanca, paint);
+
+    // Caja Negra
+    paint.color = Colors.black;
+    final cajaNegra = Rect.fromLTWH(40, 20, 70, 80);
+    canvas.drawRect(cajaNegra, paint);
   }
 
   @override
